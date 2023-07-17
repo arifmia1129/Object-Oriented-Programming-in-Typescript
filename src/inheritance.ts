8ab2bbe7408662c754e7ad295dfb0a5004fe9496
arifmia1129 <arifmia1129@gmail.com>
2023-07-17 07:34:47 +0600

    inheritance

---
+++ src/inheritance.ts
@@ -0,0 +1,47 @@
+class PersonInfo {
+  name: string;
+  age: number;
+  address: string;
+
+  constructor(name: string, age: number, address: string) {
+    this.name = name;
+    this.age = age;
+    this.address = address;
+  }
+
+  makeSleep(hours: number): string {
+    return `This ${this.name} will make sleep ${hours} hours.`;
+  }
+}
+
+class Student extends PersonInfo {
+  constructor(name: string, age: number, address: string) {
+    super(name, age, address);
+  }
+}
+
+const student1 = new Student('Ariba', 12, 'Gobindaganj, Gaibandha');
+
+console.log(student1.makeSleep(8));
+
+class Teacher extends PersonInfo {
+  designation: string;
+
+  constructor(name: string, age: number, address: string, designation: string) {
+    super(name, age, address);
+    this.designation = designation;
+  }
+
+  takeClasses(numberOfClass: number): string {
+    return `This ${this.name} take ${numberOfClass} classes in everyday`;
+  }
+}
+
+const teacher1 = new Teacher(
+  'Arif',
+  23,
+  'Gobindaganj, Gaibandha',
+  'Computer Teacher'
+);
+
+console.log(teacher1.takeClasses(7));
