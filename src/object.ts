80667c9d520bb27e8401c2ced2223a91e6f9262f
arifmia1129 <arifmia1129@gmail.com>
2023-07-17 07:04:46 +0600

    make class, class method, instance, parameter properties

---
+++ src/object.ts
@@ -0,0 +1,37 @@
+class Animal {
+  name: string;
+  species: string;
+  sound: string;
+
+  constructor(name: string, species: string, sound: string) {
+    this.name = name;
+    this.species = species;
+    this.sound = sound;
+  }
+
+  makeSound() {
+    console.log(`The ${this.name} says ${this.sound}`);
+  }
+}
+
+const dog = new Animal('German Separd', 'dog', 'Ghew Ghew');
+
+dog.makeSound();
+
+class Person {
+  constructor(public name: string, public age: number) {}
+
+  isAdult() {
+    if (this.age >= 18) {
+      console.log(`Hey I am ${this.name}. I am a adult person`);
+    } else {
+      console.log(`Hey I am ${this.name}. I am not a adult person`);
+    }
+  }
+}
+
+const ariba = new Person('Ariba', 1);
+ariba.isAdult();
+
+const arif = new Person('Arif', 23);
+arif.isAdult();
